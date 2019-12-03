e4e26f15f87970ce74a676a95aab08785eb44b79
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-02 17:05:34 -0700

    Add POST method to create new orders

---
+++ CoffeeShopSwiftUI/CoffeeShopSwiftUI/Models/CreateOrderResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CreateOrderResponse.swift
+//  CoffeeShopSwiftUI
+//
+//  Created by projas on 12/2/19.
+//  Copyright © 2019 projas. All rights reserved.
+//
+
+import Foundation
+
+struct CreateOrderResponse: Decodable {
+  let success: Bool
+}
--- CoffeeShopSwiftUI/CoffeeShopSwiftUI/Services/WebService.swift
+++ CoffeeShopSwiftUI/CoffeeShopSwiftUI/Services/WebService.swift
@@ -9,9 +9,40 @@
 import Foundation
 
 class WebService {
+  
+  private let stringUrl = "https://island-bramble.glitch.me/orders"
+  
+  func createCoffeeOrder(order: Order, completion: @escaping (CreateOrderResponse?) -> Void) {
+    
+    guard let url = URL(string: stringUrl) else {
+      fatalError("Invalid URL")
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = try? JSONEncoder().encode(order)
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+      
+      guard let data = data, error == nil else {
+        DispatchQueue.main.async {
+          completion(nil)
+        }
+        return
+      }
+      
+      let createOrderResponse = try? JSONDecoder().decode(CreateOrderResponse.self, from: data)
+      DispatchQueue.main.async {
+        completion(createOrderResponse)
+      }
+      
+    }.resume()
+  }
+  
   func getAllOrders(completion: @escaping ([Order]?) -> Void) {
     
-    guard let url = URL(string: "https://island-bramble.glitch.me/orders") else {
+    guard let url = URL(string: stringUrl) else {
       completion(nil)
       return
     }
